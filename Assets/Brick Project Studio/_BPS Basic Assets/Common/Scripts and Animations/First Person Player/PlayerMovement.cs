572c218965d2df4197b563c0e34a3b5b49d307da
Dipperra <kt1luarrackzz1xr0s@gmail.com>
2025-05-19 19:33:03 +0300

    restaurant
    commit

---
+++ Assets/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/First Person Player/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SojaExiles
+
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+
+        public CharacterController controller;
+
+        public float walkSpeed = 5f;
+        public float shiftSpeed = 10f;
+        public float gravity = -15f;
+
+        Vector3 velocity;
+
+        bool isGrounded;
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+            float speed = Input.GetKey(KeyCode.LeftShift) ? shiftSpeed : walkSpeed;
+
+            Vector3 move = transform.right * x + transform.forward * z;
+
+            controller.Move(move * speed * Time.deltaTime);
+
+            velocity.y += gravity * Time.deltaTime;
+
+            controller.Move(velocity * Time.deltaTime);
+
+
+
+        }
+    }
+}
